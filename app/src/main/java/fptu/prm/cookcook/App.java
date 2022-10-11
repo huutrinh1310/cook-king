89b150249139858f27f59ca6a4964554f9fb34f3
huutrinh1310 <huutrinh1310@gmail.com>
2022-10-11 17:59:01 +0700

    re-initial project

---
+++ app/src/main/java/fptu/prm/cookcook/App.java
@@ -0,0 +1,22 @@
+package fptu.prm.cookcook;
+
+public class App extends android.app.Application {
+    private static App mInstance;
+    private static com.google.gson.Gson mGson;
+
+    public static App self() {
+        return mInstance;
+    }
+
+    public static com.google.gson.Gson getGSon() {
+        return mGson;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+        mGson = new com.google.gson.Gson();
+    }
+
+}
