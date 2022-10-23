7397df06e6efd53a5b22cdb9496d2965b7fc93a9
namnn <namnnhe150694@fpt.edu.vn>
2022-10-24 00:22:52 +0700

    google sign in

--- app/src/main/java/fptu/prm/cookcook/ui/activity/MainActivity.java
+++ app/src/main/java/fptu/prm/cookcook/ui/activity/MainActivity.java
@@ -2,18 +2,31 @@ package fptu.prm.cookcook.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import fptu.prm.cookcook.R;
+import fptu.prm.cookcook.databinding.ActivityMainBinding;
 import fptu.prm.cookcook.utils.ToastUtil;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mViewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mViewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = mViewBinding.getRoot();
+        setContentView(view);
 
+        mViewBinding.btnSignOut.setOnClickListener(view1 -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(this, SplashActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
--- app/src/main/java/fptu/prm/cookcook/ui/activity/SignInActivity.java
+++ app/src/main/java/fptu/prm/cookcook/ui/activity/SignInActivity.java
@@ -3,14 +3,31 @@ package fptu.prm.cookcook.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import fptu.prm.cookcook.R;
 import fptu.prm.cookcook.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
     private ActivitySignInBinding mViewBinding;
+    private GoogleSignInClient mClient;
+    private GoogleSignInOptions mOptions;
+    private FirebaseAuth mAuth;
+    private final int REQUEST_CODE_SIGN_IN = 200;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,7 +35,12 @@ public class SignInActivity extends AppCompatActivity {
         mViewBinding = ActivitySignInBinding.inflate(getLayoutInflater());
         View view = mViewBinding.getRoot();
         setContentView(view);
-        
+        mOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mClient = GoogleSignIn.getClient(this, mOptions);
+        mAuth = FirebaseAuth.getInstance();
         initView();
         initControl();
     }
@@ -29,6 +51,11 @@ public class SignInActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+        mViewBinding.btnSignInWithGoogle.setOnClickListener(view -> {
+            Intent intent = mClient.getSignInIntent();
+            startActivityForResult(intent, REQUEST_CODE_SIGN_IN);
+        });
+
         mViewBinding.tvSignUp.setOnClickListener(view -> {
             Intent intent = new Intent(this, SignUpActivity.class);
             startActivity(intent);
@@ -37,5 +64,40 @@ public class SignInActivity extends AppCompatActivity {
 
     private void initView() {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                mAuth.signInWithCredential(credential)
+                        .addOnCompleteListener(task1 -> {
+                            if (task1.isSuccessful()) {
+                                Toast.makeText(this, "Sign in successful, welcome "+ account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(this, MainActivity.class);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(this, task1.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } catch (ApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 }
